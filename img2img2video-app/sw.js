2c5790ceddb7a1f9c4acbdf4c3cac87f4c315675
danburdi2015-jpg <danburdi2015@gmail.com>
2026-03-22 16:54:48 -0400

    Create sw.js file for offline support and caching

---
+++ img2img2video-app/sw.js
@@ -0,0 +1,51 @@
+// sw.js - A simple Service Worker to enable offline support and cache app files
+
+const CACHE_NAME = 'my-app-cache-v1';
+const urlsToCache = [
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/img/logo.png',
+    // Add other files you want to cache
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
